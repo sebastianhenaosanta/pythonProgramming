95410972749e83830391fd456127dc9568fece97
sebastianhenaosanta <sebastianhenao7@gmail.com>
2020-02-20 12:51:07 -0500

    first commit

--- pythonBasics/pythonSintax/helloWorld.py
+++ pythonBasics/pythonSintax/helloWorld.py
@@ -2,10 +2,17 @@
 
 
 """
-If programming is the act of teaching a computer to have a conversation with a user, it would be most useful to first teach the computer how to speak. In Python, this is accomplished with the print statement.
+If programming is the act of teaching a computer to have a conversation with a user, 
+it would be most useful to first teach the computer how to speak. In Python,
+ this is accomplished with the print statement.
 
+#code 
+print("Hello, world!")
+print("Water—there is not a drop of water there! Were Niagara but a cataract of sand, would you travel your thousand miles to see it?")
+#code
 
-A print statement is the easiest way to get your Python program to communicate with you. Being able to command this communication will be one of the most valuable tools in your programming toolbox.
+A print statement is the easiest way to get your Python program to communicate with you. 
+Being able to command this communication will be one of the most valuable tools in your programming toolbox.
 
 
 Exercise
