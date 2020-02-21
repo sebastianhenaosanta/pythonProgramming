b59efbf4340ba49de01a08fe91c9724ea6983d6f
sebastianhenaosanta <sebastianhenao7@gmail.com>
2020-02-21 14:56:00 -0500

    Creating variables in python

---
+++ pythonBasics/pythonSintax/variables.py
@@ -0,0 +1,21 @@
+#Variables
+
+
+"""
+In Python, and when programming in general, we need to build systems for dealing with data that changes over time. That data could be the location of a plane, or the time of day, or the television show you’re currently watching. The only important thing is that it may be different at different times. Python uses variables to define things that are subject to change.
+
+#code 
+greeting_message = "Welcome to the world of programming!"
+current_excercise = 5
+#code
+
+In the above example, we defined a variable called greeting_message and set it equal to the string “Welcome to programming!”. It also defined a variable called current_exercise and set it equal to the number 5.
+
+
+#EXERCICE
+Create a variable called todays_date and assign a value that will represent today’s date to that variable.
+
+
+"""
+todays_date = 21
+
