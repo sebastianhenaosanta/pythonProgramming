a3e57036b0a9afcb4df6a58d8fc8b1cc27fb60c9
sebastianhenaosanta <sebastianhenao7@gmail.com>
2020-02-21 13:38:23 -0500

    strings in python

---
+++ pythonBasics/pythonSintax/strings.py
@@ -0,0 +1,24 @@
+#Strings in python
+"""
+When printing things in Python, we are supplying a text block that we want to be printed. Text in Python is considered a specific type of data called a string. A string, so named because they’re a series of letters, numbers, or symbols connected in order — as if threaded together by string. Strings can be defined in different ways:
+#code
+print("This is a good string")
+print('You can use single quotes or double quotes for a string')
+#code
+
+Above we printed two things that are strings and then attempted to print two things that are not strings. While double-quotes (“) and single-quotes (‘) are both acceptable ways to define a string, a string needs to be opened and closed by the same type of quote mark.
+
+We can combine multiple strings using +, like so:
+
+#code
+print("This is " + "a good string")
+#code
+
+This code will print out “This is a good string”
+
+Exercise
+
+Try adding your name to the print statement with the + operator so that this Python program prints “Hello [your_name]”
+"""
+
+print("Hello "+"sebastian")
