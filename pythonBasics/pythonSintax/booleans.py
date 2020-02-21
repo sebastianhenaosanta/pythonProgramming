83077bfb8cf9dd995fbb7b20a5016c86f42f96da
sebastianhenaosanta <sebastianhenao7@gmail.com>
2020-02-21 15:20:26 -0500

    boolean data type in python

---
+++ pythonBasics/pythonSintax/booleans.py
@@ -0,0 +1,25 @@
+#Booleans
+"""
+Sometimes we have a need for variables that are either true or false. This datatype, which can only ever take one of two values, is called a boolean. In Python, we define booleans using the keywords True and False:
+
+#code
+a = True
+b = False
+#code
+
+A boolean is actually a special case of an integer. A value of True corresponds to an integer value of 1, and will behave the same. A value of False corresponds to an integer value of 0.
+
+#Exerecice
+
+1*Read the comments and then create a variable called age_is_12 and set it to be True or False depending on if this person’s age is 12.
+
+2*Create a variable called name_is_maria and set it to be True or False depending on if this person’s name is Maria.
+
+"""
+
+# Hi! I'm Maria and I live in script.py.
+# I'm an expert Python coder.
+# I'm 21 years old and I plan to program cool stuff forever.
+
+age_is_12 = False
+name_is_maria = True
