92af5ad3f6f07afc8f6c55aa1b5d7657622f54db
sebastianhenaosanta <sebastianhenao7@gmail.com>
2020-02-25 20:10:06 -0500

    string methods in python (len, lower, upper, str)

---
+++ pythonBasics/strings/StringMethods.py
@@ -0,0 +1,47 @@
+"""
+String methods
+
+Great work! Now that we know how to store strings, let’s see how we can change them using string methods.
+
+String methods let you perform specific tasks for strings.
+
+We’ll focus on four string methods:
+
+    len()
+    lower()
+    upper()
+    str()
+
+Let’s start with len(), which gets the length (the number of characters) of a string!
+
+
+Exercise
+
+On line 1, create a variable named parrot and set it to the string "Norwegian Blue".
+
+On line 2, type len(parrot) after the word print, like so: print len(parrot). The output will be the number of characters in "Norwegian Blue"!
+
+"""
+.
+0
+parrot = "Norwegian Blue"
+pi = 3.14
+
+print(len(parrot))#size of the string variable
+print(parrot.lower())#convers in lower case all the letters in the string
+print(parrot.upper())#convers in upper case all the letters in the string
+print(str(pi))#convert a numerical variable into a string  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
