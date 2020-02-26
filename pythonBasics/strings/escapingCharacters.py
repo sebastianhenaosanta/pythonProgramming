669101f2718898f27be3e8086a2e608d83fc99c7
sebastianhenaosanta <sebastianhenao7@gmail.com>
2020-02-25 19:19:55 -0500

    first commit

---
+++ pythonBasics/strings/escapingCharacters.py
@@ -0,0 +1,26 @@
+
+"""
+Escaping characters
+
+There are some characters that cause problems. For example:
+
+'There's a snake in my boot!'
+
+This code breaks because Python thinks the apostrophe in 'There's' ends the string. We can use the backslash to fix the problem, like this:
+
+'There\'s a snake in my boot!'
+
+""""
+
+print('This isn\'t flying, this is falling with style!')
+
+
+
+
+
+
+
+
+
+
+
---
+++ pythonBasics/strings/strings.py
@@ -0,0 +1,23 @@
+"""
+Strings
+
+Another useful data type is the string. A string can contain letters, numbers, and symbols.
+
+name = "Ryan"
+age = "19"
+food = "cheese"
+    In the above example, we create a variable name and set it to the string value "Ryan".
+    We also set age to "19" and food to "cheese".
+
+"""
+
+brian = "Hello life!"
+
+
+
+
+
+
+
+
+
