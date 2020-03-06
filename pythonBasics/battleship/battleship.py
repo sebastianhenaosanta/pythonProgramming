cb50ea920fb56d8ad2df20cc48a3607e0a9f1054
sebastianhenaosanta <sebastianhenao7@gmail.com>
2020-03-06 16:36:19 -0500

    battleship programm

---
+++ pythonBasics/battleship/battleship.py
@@ -0,0 +1,10 @@
+"""
+
+Welcome to Battleship!
+
+In this project you will build a simplified, one-player version of the classic board game Battleship! In this version of the game, there will be a single ship hidden in a random location on a 5x5 grid. The player will have 10 guesses to try to sink the ship.
+
+To build this game we will use our knowledge of lists, conditionals and functions in Python. 
+"""
+board = []
+
